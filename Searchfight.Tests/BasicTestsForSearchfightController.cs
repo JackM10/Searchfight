de84fa6e4adeeda4faad607ced038a93453a4562
Jack <jack_10@mail.ru>
2020-07-01 19:49:45 +0300

    small refactorin - file renaming

--- Searchfight.Tests/BasicTestsForSearchfightController.cs
+++ Searchfight.Tests/BasicTestsForSearchfightController.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Searchfight.Controllers;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using SearchfightDAL;
